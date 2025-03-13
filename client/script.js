c3ffaad039f4137a91944d9cae67250f7c6957fa
Diego Marquez <diegom75@nycstudents.net>
2025-03-13 14:22:42 +0000

    added display function

--- client/script.js
+++ client/script.js
@@ -18,7 +18,22 @@ const url = "/api/todos";
 
 // Display todos in the list
 function displayTodos(todos) {
-    toDoListDisplay = document.getElementsById(toDoList);
+    todoList.innerHTML = ""; // clear existing todos
+    todos.forEach(todo => {
+        // creates a new list item, so that it gets added to the todoList, then sets the text content to the todo title
+        const li= document.createElement("li");
+        li.textContent = todo.title
+        // create delete button
+        const deleteButton = document.createElement("button");
+        deleteButton.textContent="Delete";
+
+        //function to delete todos
+        deleteButton.onclick = () => {
+          deleteTodo(todo.id); // the id is there so that the todos can be deleted correctly
+        };
+        li.appendChild(deleteButton); // appends the delete button to the llist item.
+        todoList.appendChild(li);// appends the last item to the todo list 
+  });
 }
 
 // Handle form submission
